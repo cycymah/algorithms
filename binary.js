36eae17da4e2b69ea3e1fe102a7b34234decd706
Ivan Solnyshkin <ethosy@gmail.com>
2023-05-30 20:14:14 +0700

    правки

--- binary.js
+++ binary.js
@@ -1,49 +1,52 @@
 const nums = [];
+const generateNumsCount = 10000000;
+const findNumber = 10000000;
 
-for (let i = 0; i < 10000000; i++) {
-    nums.push({ value: i, name: `Число ${i}` });
+// Создаем список по которому проходмся
+for (let i = 0; i <= generateNumsCount; i++) {
+  nums.push({value: i, name: `Число ${i}`});
 }
 
 // Поиск стандартными методами arr
-const findNumberRegular = (num) => nums.find((n) => n.value === num).name;
+const findNumberRegular = (num) => nums.find((n) => n.value === num)?.name ?? 'Не найдено';
 
 // Поиск перебором
 const customRecular = (num) => {
-    for (let i = 0; i <= nums.length; i++) {
-        if (nums[i].value === num) return nums[i].name;
-    }
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i].value === num) return nums[i].name
+  }
 };
 
 // Бинарный поиск
 const binaryFind = (num) => {
-    let start = 0;
-    let end = nums.length - 1;
+  let start = 0;
+  let end = nums.length - 1;
 
-    while (start <= end) {
-        const mid = Math.floor((start + end) / 2);
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
 
-        if (nums[mid].value === num) {
-            return nums[mid].name;
-        } else if (nums[mid].value < num) {
-            start = mid + 1;
-        } else {
-            end = mid - 1;
-        }
+    if (nums[mid].value === num) {
+      return nums[mid].name;
+    } else if (nums[mid].value < num) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
     }
+  }
 
-    return 'Не найдено';
+  return 'Не найдено';
 };
 
 console.time('regular');
-const findedRegular = findNumberRegular(400000);
+const findedRegular = findNumberRegular(findNumber);
 console.timeEnd('regular');
 
 console.time('regularCustom');
-const findedCustomRegular = customRecular(400000);
+const findedCustomRegular = customRecular(findNumber);
 console.timeEnd('regularCustom');
 
 console.time('binary');
-const binaryFindRes = binaryFind(400000);
+const binaryFindRes = binaryFind(findNumber);
 console.timeEnd('binary');
 
 console.log(findedRegular, findedCustomRegular, binaryFindRes);
