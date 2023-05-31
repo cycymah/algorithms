f38f734f5adc8e6959098a9db38aa1010710d5f8
Ivan Solnyshkin <ethosy@gmail.com>
2023-05-31 12:20:14 +0700

    Быстрая сортировка рекурсией

--- src/sort.js
+++ src/sort.js
@@ -1,12 +1,12 @@
 const arrToSort = [];
-const generateSortNumsCount = 10;
+const generateSortNumsCount = 100000;
 
 // Создаем список по которому проходмся
 for (let i = 0; i <= generateSortNumsCount; i++) {
   arrToSort.push(Math.round(Math.random() * 10));
 }
 
-
+// Сортировка выбором
 const sortCustom = (arr) => {
   for (let i = 0; i < arr.length - 1; i++) {
     let minIndex = i;
@@ -21,6 +21,26 @@ const sortCustom = (arr) => {
   return arr;
 }
 
+// Быстрая сортировка с применением рекурсии и принципом Разделяй и влавствуй
+const quickSort = (arr) => {
+  if (arr.length <= 1) return arr
+
+  const refValue = arr[Math.floor(arr.length / 2)];
+  const left = [];
+  const right = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    // Разбиваем на подмассивы
+    if (arr[i] < refValue) {
+      left.push(arr[i]);
+    } else if (arr[i] > refValue) {
+      right.push(arr[i]);
+    }
+  }
+
+  return [...quickSort(left), refValue, ...quickSort(right)];
+}
+
 console.time('regular')
 const regularResult = arrToSort.sort((a, b) => a - b)
 console.timeEnd('regular')
@@ -29,5 +49,8 @@ console.time('customSort')
 const customResult = sortCustom(arrToSort)
 console.timeEnd('customSort')
 
-console.log(arrToSort, regularResult, customResult)
+console.time('quickSort')
+const quickSortResult = quickSort(arrToSort)
+console.timeEnd('quickSort')
+
 
